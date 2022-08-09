91437972fc305742f9a587415f0ab5a375daa2f0
Pablo de la Peña <p@shedcollective.org>
2022-08-09 11:54:12 +0100

    chore: Correct overload namespace

--- services/services.php
+++ services/services.php
@@ -5,15 +5,15 @@ use Nails\Deploy\Factory;
 return [
     'factories' => [
         'EmailAlertPre'  => function (): Factory\Email\Alert\Pre {
-            if (class_exists('\App\Admin\Factory\Email\Alert\Pre')) {
-                return new \App\Admin\Factory\Email\Alert\Pre();
+            if (class_exists('\App\Deploy\Factory\Email\Alert\Pre')) {
+                return new \App\Deploy\Factory\Email\Alert\Pre();
             } else {
                 return new Factory\Email\Alert\Pre();
             }
         },
         'EmailAlertPost' => function (): Factory\Email\Alert\Post {
-            if (class_exists('\App\Admin\Factory\Email\Alert\Post')) {
-                return new \App\Admin\Factory\Email\Alert\Post();
+            if (class_exists('\App\Deploy\Factory\Email\Alert\Post')) {
+                return new \App\Deploy\Factory\Email\Alert\Post();
             } else {
                 return new Factory\Email\Alert\Post();
             }
